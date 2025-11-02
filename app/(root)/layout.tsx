7631a88c634ca555d075d1712eb7fec639d12d9d
sital-tharu <sitatharu769@gmail.com>
2025-11-02 19:20:13 +0530

    handaling layout for the different routes

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const  Layout = ({children }: {children: React.ReactNode}) => {
+    return <div>
+        <p> NavBar</p>
+        {children}
+    </div>;
+};
+
+export default Layout;
