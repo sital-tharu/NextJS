bc5fa0c918925f1513815f2f9a8e19b2bdb61080
sital-tharu <sitatharu769@gmail.com>
2025-11-03 22:42:54 +0530

    Implmenting Customized Error Handling for the Route

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,6 +1,6 @@
 
 const Page = () => {
-
+throw new Error('Not implemented')
   return <div>
       <h1> About</h1>
   </div>;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const  Layout = ({children }: {children: React.ReactNode}) => {
     return <div>
         <p> NavBar</p>
